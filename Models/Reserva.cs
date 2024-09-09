54fe775993688bbc6bddcd95dff764b89255bd52
pedrofaluba <pedrofaluba@gmail.com>
2024-09-09 17:17:09 -0300

    Desafio concluido

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,14 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // Implementado #MeContrataXP
+            // Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             if (Suite.Capacidade >= hospedes.Count())
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // Implementado #MeContrataXP
+                // Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 throw new Exception("O número de hóspedes supera a capacidade da suíte desejada.");
             }
         }
@@ -36,20 +34,17 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // Implementado #MeContrataXP
+            // Retorna a quantidade de hóspedes (propriedade Hospedes)
             return Hospedes.Count();
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
+            // Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // Implementado #MeContrataXP
             decimal valor = DiasReservados*Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // Implementado #MeContrataXP
             decimal desconto = .10M;
             if (DiasReservados >= 10)
             {
